661d7a73af0fb7f90ad06941b764eca81c83fbd6
DolXeGoD <dolxegod@gmail.com>
2018-06-05 03:08:01 +0900

    added OutActivity send retrofit (imcomplete)

--- app/src/main/java/dgsw/hs/kr/flow/Activity/OutActivity.java
+++ app/src/main/java/dgsw/hs/kr/flow/Activity/OutActivity.java
@@ -4,15 +4,22 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
+import dgsw.hs.kr.flow.Model.request.Out;
+import dgsw.hs.kr.flow.Model.response.ResponseFormat;
+import dgsw.hs.kr.flow.Network.RetrofitService;
 import dgsw.hs.kr.flow.R; //R 오류가 왜 뜨는지 모르겠네 ㅠㅠ 돌려막기
+import dgsw.hs.kr.flow.Utils.APIUtills;
+import retrofit2.Call;
 
 public class OutActivity extends AppCompatActivity {
 
@@ -27,6 +34,9 @@ public class OutActivity extends AppCompatActivity {
     private String E_DATE;
     private String E_TIME;
     private String E_DATETIME;
+    private String REASON_TO_OUT;
+    private RetrofitService mRTService;
+    private Call<ResponseFormat> mResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,8 @@ public class OutActivity extends AppCompatActivity {
         Button enddate_btn = findViewById(R.id.btn_setEndDate);
         Button starttime_btn = findViewById(R.id.btn_setStartTime);
         Button endtime_btn = findViewById(R.id.btn_setEndTime);
+        Button submit_btn = findViewById(R.id.btn_goSubmit);
+        final EditText reason_et = findViewById(R.id.et_reason);
 
         final Calendar cal = Calendar.getInstance();
         final DatePickerDialog S_Ddialog = new DatePickerDialog(this, sdateListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
@@ -44,6 +56,7 @@ public class OutActivity extends AppCompatActivity {
         final DatePickerDialog E_Ddialog = new DatePickerDialog(this, edateListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
         final TimePickerDialog E_Tdialog = new TimePickerDialog(this, etimeListener, hour, minute, true);
 
+        //================================= OnClick Listener About DATE & TIME
         startdate_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,10 +97,29 @@ public class OutActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //==================== SUBMIT ONCLICK LISTENER
+        submit_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                REASON_TO_OUT = reason_et.getText().toString().trim();
+                Out out = new Out();
+
+                //모델 세팅 전 값 비었는지 검사.
+                if(S_DATETIME!=null&&E_DATETIME!=null&&!TextUtils.isEmpty(REASON_TO_OUT)){
+                    out.setStart_time(S_DATETIME);
+                    out.setEnd_time(E_DATETIME);
+                    out.setReason(REASON_TO_OUT);
+
+                    mRTService = APIUtills.getAPIService();
+                    mResponse = mRTService.
+                }
+            }
+        });
     }
 
 
-
+    //=========================================== onDataSet Listener
     private DatePickerDialog.OnDateSetListener sdateListener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int thisYear, int monthOfYear, int dayOfMonth) {
@@ -136,4 +168,6 @@ public class OutActivity extends AppCompatActivity {
             E_DATETIME = E_DATE+" "+E_TIME;
         }
     };
+
+
 }
