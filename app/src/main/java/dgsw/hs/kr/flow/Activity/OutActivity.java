a7b4eaa01e5febaa3544090f691dc16df8272fc7
DolXeGoD <dolxegod@gmail.com>
2018-06-05 02:03:00 +0900

    added datepicker&timepicker and exception

--- app/src/main/java/dgsw/hs/kr/flow/Activity/OutActivity.java
+++ app/src/main/java/dgsw/hs/kr/flow/Activity/OutActivity.java
@@ -21,6 +21,12 @@ public class OutActivity extends AppCompatActivity {
     private int day;
     private int hour;
     private int minute;
+    private String S_DATE;
+    private String S_TIME;
+    private String S_DATETIME;
+    private String E_DATE;
+    private String E_TIME;
+    private String E_DATETIME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,41 +39,88 @@ public class OutActivity extends AppCompatActivity {
         Button endtime_btn = findViewById(R.id.btn_setEndTime);
 
         final Calendar cal = Calendar.getInstance();
-        final DatePickerDialog Ddialog = new DatePickerDialog(this, dateListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
-        final TimePickerDialog Tdialog = new TimePickerDialog(this, timeListener, hour, minute, true);
+        final DatePickerDialog S_Ddialog = new DatePickerDialog(this, sdateListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
+        final TimePickerDialog S_Tdialog = new TimePickerDialog(this, stimeListener, hour, minute, true);
+        final DatePickerDialog E_Ddialog = new DatePickerDialog(this, edateListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
+        final TimePickerDialog E_Tdialog = new TimePickerDialog(this, etimeListener, hour, minute, true);
 
         startdate_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) { Ddialog.show(); }
+            public void onClick(View view) {
+                S_Ddialog.show();
+            }
+        });
+
+        starttime_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(S_DATE == null){
+                    Toast.makeText(getApplicationContext(), "시간을 설정하기 전에 날짜부터 설정해주세요.", Toast.LENGTH_SHORT).show();
+                }else{
+                    S_Tdialog.show();
+                }
+            }
         });
 
 
         enddate_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Ddialog.show();
+                if(S_DATETIME == null){
+                    Toast.makeText(getApplicationContext(), "시작 날짜 및 시간부터 선택해주세요.", Toast.LENGTH_SHORT).show();
+                } else{
+                    E_Ddialog.show();
+                }
+            }
+        });
+
+        endtime_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(E_DATE == null){
+                    Toast.makeText(getApplicationContext(), "시간을 설정하기 전에 날짜부터 설정해주세요.", Toast.LENGTH_SHORT).show();
+                } else{
+                    E_Tdialog.show();
+                }
             }
         });
     }
 
 
 
-    private DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+    private DatePickerDialog.OnDateSetListener sdateListener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int thisYear, int monthOfYear, int dayOfMonth) {
-            /*Toast.makeText(getApplicationContext(), thisYear + "년" + monthOfYear + "월" + dayOfMonth +"일", Toast.LENGTH_SHORT).show();*/
             year = thisYear;
             month = monthOfYear + 1;
             day = dayOfMonth;
             Toast.makeText(getApplicationContext(), year +
                     "년" + month + "월" + day+"일", Toast.LENGTH_SHORT).show();
-            /*mt.setDate(year, month, day, hour, minute);
-            Intent mealIntent = new Intent(MainActivity.this, MealActivity.class);
-            startActivity(mealIntent);*/
         }
     };
 
-    private TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() {
+    private TimePickerDialog.OnTimeSetListener stimeListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int h, int m) {
+            hour = h;
+            minute = m;
+            Toast.makeText(getApplicationContext(), hour + " 시" + minute +
+                    " 분 입니다.", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener edateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int thisYear, int monthOfYear, int dayOfMonth) {
+            year = thisYear;
+            month = monthOfYear + 1;
+            day = dayOfMonth;
+            Toast.makeText(getApplicationContext(), year +
+                    "년" + month + "월" + day+"일", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener etimeListener = new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker timePicker, int h, int m) {
             hour = h;
