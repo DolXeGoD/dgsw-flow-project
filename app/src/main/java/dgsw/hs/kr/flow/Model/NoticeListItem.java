2b15e6fe9d607a79ebfceb3328c1edfec9dd46fa
DolXeGoD <dolxegod@gmail.com>
2018-06-22 12:39:00 +0900

    Add NoticeListItem for ListView Adapter Model

---
+++ app/src/main/java/dgsw/hs/kr/flow/Model/NoticeListItem.java
@@ -0,0 +1,17 @@
+package dgsw.hs.kr.flow.Model;
+
+/**
+ * Created by 조현재 on 2018-06-21.
+ */
+
+public class NoticeListItem {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public NoticeListItem(String title){
+        this.title = title;
+    }
+}
