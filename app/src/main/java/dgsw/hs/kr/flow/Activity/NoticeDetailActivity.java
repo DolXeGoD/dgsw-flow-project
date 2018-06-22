219bb3ea2fcab4280485cb15c20d31de12cc0fa2
DolXeGoD <dolxegod@gmail.com>
2018-06-22 16:49:30 +0900

    Add NoticeDetailActivity

---
+++ app/src/main/java/dgsw/hs/kr/flow/Activity/NoticeDetailActivity.java
@@ -0,0 +1,15 @@
+package dgsw.hs.kr.flow.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dgsw.hs.kr.flow.R;
+
+public class NoticeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_detail);
+    }
+}
