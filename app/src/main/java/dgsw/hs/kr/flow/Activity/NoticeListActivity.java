3f28ed543a3c5fa7b56aed99b371fb04f9c0bda3
DolXeGoD <dolxegod@gmail.com>
2018-06-22 13:33:30 +0900

    Add NoticeListActivity

--- app/src/main/java/dgsw/hs/kr/flow/Activity/NoticeListActivity.java
+++ app/src/main/java/dgsw/hs/kr/flow/Activity/NoticeListActivity.java
@@ -3,12 +3,14 @@ package dgsw.hs.kr.flow.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
+import dgsw.hs.kr.flow.Adapter.NoticeListAdapter;
+import dgsw.hs.kr.flow.Model.NoticeListItem;
 import dgsw.hs.kr.flow.Model.response.ResponseFormat;
 import dgsw.hs.kr.flow.Network.RetrofitService;
 import dgsw.hs.kr.flow.R;
@@ -18,24 +20,63 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NoticeListActivity extends AppCompatActivity {
-    private TextView tv_result;
-    private Button btn_getnotice;
+    private ListView lv_notice;
     private RetrofitService mRTService;
     private Call<ResponseFormat> mResponse;
     private static final String TAG = "NoticeListActivity";
+    private String USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiIwMDB3aGd1c3dvQGRnc3cuaHMua3IiLCJjbGFzc0lkeCI6MiwiYXV0aCI6MiwiaWF0IjoxNTI5NjMwNzYxLCJleHAiOjE1MzAyMzU1NjEsImlzcyI6ImplZmZjaG9pLmNvbSIsInN1YiI6InRva2VuIn0.3_bcgVb0bSyQXA4qQx_7qH1wANS13G69lmCx8s3GL8o";
+    private String result_data;
+    private int NOTICE_COUNT;
 
-    private String USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiIwMDB3aGd1c3dvQGRnc3cuaHMua3IiLCJjbGFzc0lkeCI6MiwiYXV0aCI6MiwiaWF0IjoxNTI5MzcwNTYxLCJleHAiOjE1Mjk5NzUzNjEsImlzcyI6ImplZmZjaG9pLmNvbSIsInN1YiI6InRva2VuIn0.k-R39u26IjDFCQumuYFEmP9_ZMT7m27NJ3QLnhDCRHs";
-    private String RESULT_DATA;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_list);
-        btn_getnotice = findViewById(R.id.btn_get_notice);
-        tv_result = findViewById(R.id.tv_result_notice);
+        lv_notice = findViewById(R.id.lv_notice_list);
+        final ArrayList<NoticeListItem> data = new ArrayList<>();
+        final NoticeListAdapter adapter = new NoticeListAdapter(this, R.layout.item,data);
 
-        btn_getnotice.setOnClickListener(new View.OnClickListener() {
+        mRTService = APIUtills.getAPIService();
+        mResponse = mRTService.noticeGet(USER_TOKEN);
+        mResponse.enqueue(new Callback<ResponseFormat>() {
+            @Override
+            public void onResponse(Call<ResponseFormat> call, Response<ResponseFormat> response) {
+                if(response.isSuccessful()){
+                    Log.i(TAG, "response msg : " + response.message().toString());
+                    Log.i(TAG, "response code : " + response.code());
+                    Log.i(TAG, "server msg : " + response.body().getMessage());
+                    Log.i(TAG, "server code : " + response.body().getStatus());
+                    if(response.body().getStatus() == 200){
+                        NOTICE_COUNT = response.body().getResponseFormatData().getList().length;
+
+                        for(int i=0; i < 10;i++) {
+                            result_data = response.body().getResponseFormatData().getList()[i].getContent();
+                            Log.i(TAG, "CONTENT OF NOTICE : " + response.body().getResponseFormatData().getList()[i].getContent());
+                            Log.i(TAG, "NOTICE COUNT VALUE : " + NOTICE_COUNT);
+
+                            NoticeListItem noticeListItem = new NoticeListItem(result_data);
+                            data.add(noticeListItem);
+                        }
+                        lv_notice.setAdapter(adapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseFormat> call, Throwable t) {
+                Log.i(TAG, "onFailure에 걸림");
+            }
+        });
+
+        /*
+        =============================== TEST CODE ===============================
+        */
+
+        /*btn_getnotice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //retrofit.
+
                 mRTService = APIUtills.getAPIService();
                 mResponse = mRTService.noticeGet(USER_TOKEN);
                 mResponse.enqueue(new Callback<ResponseFormat>() {
@@ -47,10 +88,23 @@ public class NoticeListActivity extends AppCompatActivity {
                             Log.i(TAG, "server msg : " + response.body().getMessage());
                             Log.i(TAG, "server code : " + response.body().getStatus());
                             if(response.body().getStatus() == 200){
-                                Log.i(TAG, "DATATHISISHOLYDATA -> " + response.body());
+                                NOTICE_COUNT = response.body().getResponseFormatData().getList().length;
+
+                                for(int i=0; i < 10;i++) {
+                                    result_data = response.body().getResponseFormatData().getList()[i].getContent();
+                                    Log.i(TAG, "CONTENT OF NOTICE : " + response.body().getResponseFormatData().getList()[i].getContent());
+                                    Log.i(TAG, "NOTICE COUNT VALUE : " + NOTICE_COUNT);
+
+                                    NoticeListItem noticeListItem = new NoticeListItem(result_data);
+                                    data.add(noticeListItem);
+                                }
+                                lv_notice.setAdapter(adapter);
+                                *//*
                                 RESULT_DATA = response.body().getResponseFormatData().getList()[0].getContent();
+                                Log.i(TAG, "총 공지의 갯수입니다. : " + response.body().getResponseFormatData().getList().length);
                                 tv_result.setText(RESULT_DATA);
                                 Log.i(TAG, RESULT_DATA);
+                                *//*
                             }
                         }
                     }
@@ -61,6 +115,6 @@ public class NoticeListActivity extends AppCompatActivity {
                     }
                 });
             }
-        });
+        });*/
     }
 }
