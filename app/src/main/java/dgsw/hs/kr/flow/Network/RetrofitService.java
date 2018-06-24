b1ab102eb63d616e76093cdded5c38f7ca640a9e
DolXeGoD <dolxegod@gmail.com>
2018-06-25 08:49:12 +0900

    Add getNoticeIdx Service

--- app/src/main/java/dgsw/hs/kr/flow/Network/RetrofitService.java
+++ app/src/main/java/dgsw/hs/kr/flow/Network/RetrofitService.java
@@ -27,6 +27,9 @@ public interface RetrofitService {
     @POST("out/sleep")
     Call<ResponseFormat> sleepOutPost(@Body Out out, @Header("x-access-token") String token);
 
+    @GET("notice")
+    Call<ResponseFormat> noticeGet(@Header("x-access-token") String token);
+
     @GET("notice/{idx}")
-    Call<ResponseFormat> noticeGet(@Body int idx,@Header("x-access-token") String token);
+    Call<ResponseFormat> noticeDetailGet(@Body int idx,@Header("x-access-token") String token);
 }
