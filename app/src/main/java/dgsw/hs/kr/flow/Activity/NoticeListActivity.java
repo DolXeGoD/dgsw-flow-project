9bfc4d981eda4ee9c330700ba835124cf6e48db0
DolXeGoD <dolxegod@gmail.com>
2018-06-25 08:48:36 +0900

    Delete Useless comment

--- app/src/main/java/dgsw/hs/kr/flow/Activity/NoticeListActivity.java
+++ app/src/main/java/dgsw/hs/kr/flow/Activity/NoticeListActivity.java
@@ -1,8 +1,11 @@
 package dgsw.hs.kr.flow.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -52,8 +55,9 @@ public class NoticeListActivity extends AppCompatActivity {
                             Log.i(TAG, "NOTICE COUNT VALUE : " + NOTICE_COUNT);
                             NoticeListItem noticeListItem = new NoticeListItem(result_data);
                             data.add(noticeListItem);
-                            lv_notice.setAdapter(adapter);
                         }
+                        lv_notice.setAdapter(adapter);
+                        lv_notice.setOnItemClickListener(mItemClickListener);
                     }
                 }
             }
@@ -63,56 +67,15 @@ public class NoticeListActivity extends AppCompatActivity {
                 Log.i(TAG, "onFailure에 걸림");
             }
         });
-
-        /*
-        =============================== TEST CODE ===============================
-        */
-
-        /*btn_getnotice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //retrofit.
-
-                mRTService = APIUtills.getAPIService();
-                mResponse = mRTService.noticeGet(USER_TOKEN);
-                mResponse.enqueue(new Callback<ResponseFormat>() {
-                    @Override
-                    public void onResponse(Call<ResponseFormat> call, Response<ResponseFormat> response) {
-                        if(response.isSuccessful()){
-                            Log.i(TAG, "response msg : " + response.message().toString());
-                            Log.i(TAG, "response code : " + response.code());
-                            Log.i(TAG, "server msg : " + response.body().getMessage());
-                            Log.i(TAG, "server code : " + response.body().getStatus());
-                            if(response.body().getStatus() == 200){
-                                NOTICE_COUNT = response.body().getResponseFormatData().getList().length;
-
-                                for(int i=0; i < 10;i++) {
-                                    result_data = response.body().getResponseFormatData().getList()[i].getContent();
-                                    Log.i(TAG, "CONTENT OF NOTICE : " + response.body().getResponseFormatData().getList()[i].getContent());
-                                    Log.i(TAG, "NOTICE COUNT VALUE : " + NOTICE_COUNT);
-
-                                    NoticeListItem noticeListItem = new NoticeListItem(result_data);
-                                    data.add(noticeListItem);
-                                }
-                                lv_notice.setAdapter(adapter);
-                                *//*
-                                RESULT_DATA = response.body().getResponseFormatData().getList()[0].getContent();
-                                Log.i(TAG, "총 공지의 갯수입니다. : " + response.body().getResponseFormatData().getList().length);
-                                tv_result.setText(RESULT_DATA);
-                                Log.i(TAG, RESULT_DATA);
-                                *//*
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseFormat> call, Throwable t) {
-                        Log.i(TAG, "onFailure에 걸림");
-                    }
-                });
-            }
-        });*/
-
-        /* ======================================================================= */
     }
+
+    private AdapterView.OnItemClickListener mItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Log.i(TAG, "POSITION VALUE : " + position);
+            Intent intent = new Intent(getApplicationContext(), NoticeDetailActivity.class);
+            intent.putExtra("notice_index", position);
+            startActivity(intent);
+        }
+    };
 }
