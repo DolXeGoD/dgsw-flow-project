ff63a9db5c0a4200b4901abe49c74ee350c6ea54
DolXeGoD <dolxegod@gmail.com>
2018-06-26 23:00:19 +0900

    Add test case in DB ManagerTestActivity

--- app/src/main/java/dgsw/hs/kr/flow/Activity/DBManagerTestActivity.java
+++ app/src/main/java/dgsw/hs/kr/flow/Activity/DBManagerTestActivity.java
@@ -7,10 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import dgsw.hs.kr.flow.Database.OutDBManager;
-import dgsw.hs.kr.flow.Database.TokenDBManager;
+import dgsw.hs.kr.flow.Database.DBManager;
 import dgsw.hs.kr.flow.R;
 
 public class DBManagerTestActivity extends AppCompatActivity {
@@ -21,8 +19,8 @@ public class DBManagerTestActivity extends AppCompatActivity {
     private String reason = "정말 놀고싶다";
     private int class_idx = 2;
     private String student_email = "dolxegod@gmail.com";
+    private int server_idx = 555;
     private static final String TAG = "DBTestActivity";
-    private String[][] array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +30,45 @@ public class DBManagerTestActivity extends AppCompatActivity {
         final Button btn_insert = findViewById(R.id.btn_insertTest);
         final Button btn_delete = findViewById(R.id.btn_delete);
         final TextView resultTv = findViewById(R.id.tv_resultOfSelect);
-        final OutDBManager outDbManager = new OutDBManager(getApplicationContext(), "FlowUser.db", null, 1);
+        final Button btn_testtokeninsert = findViewById(R.id.btn_test_insert_token);
+        final DBManager dbManager = new DBManager(getApplicationContext(), "FlowUser.db", null, 1);
         resultTv.setText("thistextisnotincludeanymeans");
 
         btn_select.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Cursor cursor;
-                cursor = outDbManager.select(1);
-                cursor.moveToNext();
-                int accept = cursor.getInt(1);
-                String start_time = cursor.getString(2);
-                String student_email = cursor.getString(6);
-                Toast.makeText(getApplicationContext(), accept + ", " + start_time + ", " + student_email,Toast.LENGTH_SHORT);
-                Log.i("fucl", accept + "" + start_time + "" + student_email);
+                int cnt = dbManager.out_count();
+
+                Log.i("COUNT VALUE", " IS : "+cnt);
             }
         });
 
         btn_insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                outDbManager.insert(accept, start_time, end_time, reason, class_idx, student_email);
+                dbManager.out_insert(accept, start_time, end_time, reason, class_idx, student_email, server_idx);
             }
         });
 
         btn_delete.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                outDbManager.delete(1);
+                for(int i = 0;i<20;i++){
+                    dbManager.out_delete(i);
+                }
+                int cnt = dbManager.out_count();
+                Log.i("Tag", "COUNT AFTER DELETE : " + cnt);
+            }
+        });
+
+        btn_testtokeninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*dbManager.token_insert("testtoken1");
+                dbManager.token_select();*/
+
+                dbManager.droptable();
             }
         });
     }
