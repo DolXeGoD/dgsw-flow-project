018ea9c2267f3e53bbb584186021f6a0d680c1db
DolXeGoD <dolxegod@gmail.com>
2018-06-28 21:03:50 +0900

    Finish OutListAdapter.java & OutListItem.java

--- app/src/main/java/dgsw/hs/kr/flow/Adapter/OutListAdapter.java
+++ app/src/main/java/dgsw/hs/kr/flow/Adapter/OutListAdapter.java
@@ -1,13 +1,16 @@
 package dgsw.hs.kr.flow.Adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import dgsw.hs.kr.flow.Model.OutListItem;
+import dgsw.hs.kr.flow.R;
 
 /**
  * Created by 조현재 on 2018-06-25.
@@ -16,24 +19,51 @@ import dgsw.hs.kr.flow.Model.OutListItem;
 public class OutListAdapter extends BaseAdapter {
     private LayoutInflater inflater;
     private ArrayList<OutListItem> data;
+    private int layout;
+
+    public OutListAdapter(Context context, int layout, ArrayList<OutListItem> data){
+        this.inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.data=data;
+        this.layout=layout;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return data.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return data.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if(convertView == null){
+            convertView = inflater.inflate(layout, parent, false);
+        }
+        OutListItem outListItem = data.get(position);
+        TextView start_time = convertView.findViewById(R.id.tv_start_time);
+        TextView end_time = convertView.findViewById(R.id.tv_end_time);
+        TextView reason = convertView.findViewById(R.id.tv_reason);
+        TextView isAccept = convertView.findViewById(R.id.tv_isAccept);
+
+        start_time.setText(outListItem.getStart_date());
+        end_time.setText(outListItem.getEnd_date());
+        reason.setText(outListItem.getReason());
+
+        if(outListItem.getAccept() == 0){
+            isAccept.setText("승인 대기중.");
+        }
+        else if(outListItem.getAccept() == 1){
+            isAccept.setText("승인됨.");
+        }
+
+        return convertView;
     }
 }
--- app/src/main/java/dgsw/hs/kr/flow/Model/OutListItem.java
+++ app/src/main/java/dgsw/hs/kr/flow/Model/OutListItem.java
@@ -11,6 +11,46 @@ public class OutListItem {
     private String reason;
     private String student_email;
 
+    public int getAccept() {
+        return accept;
+    }
+
+    public void setAccept(int accept) {
+        this.accept = accept;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStudent_email() {
+        return student_email;
+    }
+
+    public void setStudent_email(String student_email) {
+        this.student_email = student_email;
+    }
+
     public OutListItem(int accept, String start_date, String end_date, String reason, String student_email) {
         this.accept = accept;
         this.start_date = start_date;
